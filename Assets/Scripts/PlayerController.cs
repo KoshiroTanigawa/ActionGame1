6ce10e6ca0de20e230447535169f8233a64e418a
Koshiro_0507 <koushiro.hakk2525@gmail.com>
2022-07-11 01:17:50 +0900

    基底クラスItemBase,派生クラスPointCube,SplitBallのスクリプト作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _gravityScale = 1.5f;
-
+    [SerializeField][Header("速度")]
+    [Tooltip("キャラクターの移動速度")]
     public float _speed = 20f;
+
+    [SerializeField][Header("ジャンプ力")]
+    [Tooltip("キャラクターのジャンプ力")]
     public float _jumpForce = 10f;
 
-    float _maxPos = 6;
-    float _minPos = 0;
-    float _posZ;
+    [SerializeField]
+    [Header("重力スケール")]
+    [Tooltip("重力の倍率")]
+    public float _gravityScale = 1.5f;
+
+    public float[] _posZ = [0, 3, 6];
+
+    bool _isOnGround;
 
     Rigidbody _rb;
 
@@ -20,25 +27,14 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();    //Rigidbody取得
         Physics.gravity *= _gravityScale;   //重力の大きさ変更
-        _posZ = transform.position.z;
-
+        _isOnGround = true;
     }
 
     void Update()
     {
         Walk(); //等速移動
+        Jump(); //ジャンプ
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-
-        float inputVertical = Input.GetAxis("Vertical") * 3 * Time.deltaTime;
-
-        if (_posZ < _maxPos && _posZ > _minPos)
-        {
-            _posZ +=  inputVertical;
-        }
     }
 
     /// <summary>
@@ -54,6 +50,19 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     public void Jump()
     {
-        _rb.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            _rb.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+            _isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            _isOnGround = true;
+        }
     }
 }
